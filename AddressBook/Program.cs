8954aba35b139aad16c41eaa1cbf9a3a0d68669c
unknown <jimpidas>
2021-05-13 18:28:39 +0530

    [Jyosmita]Added: creating contact

---
+++ AddressBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Contacts contact = new Contacts();
+            contact.addContact();
+
+        }
+    }
+}
