deb85df680e0b70cc8cb46a1b3bf35958f3e5a8d
unknown <jimpidas>
2021-05-16 23:00:18 +0530

    [Jyosmita]Added code to add multiple address

--- AddressBook/AddressBookBuider.cs
+++ AddressBook/AddressBookBuider.cs
@@ -6,11 +6,9 @@ using System.Text;
 namespace AddressBook
 {
     class AddressBookBuider : IContacts
-    {
-
-
-
+    {       
         private LinkedList<Contacts> list = new LinkedList<Contacts>();
+        private Dictionary<string, AddressBookBuider> dictionary=new Dictionary<string,AddressBookBuider>();
         public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber)
         {
             Contacts contact = new Contacts();
@@ -29,7 +27,7 @@ namespace AddressBook
 
             if (list.Count > 0)
             {
-
+              
                 foreach (var contactlist in list)
                 {
                     Console.WriteLine("First Name : " + contactlist.FirstName);
@@ -99,16 +97,19 @@ namespace AddressBook
         }
 
 
-        public void DeleteContact(string name)
+        public void DeleteContact(string deletename)
         {
-            Contacts contact = new Contacts();
-
+          
+            
             if (list.Count > 0)
             {
-                foreach (var contactlist in list)
+                 foreach(var contact in list)
                 {
-                    if (contactlist.FirstName == name)
+                    if (contact.FirstName == deletename)
+                    {
                         list.Remove(contact);
+                        Console.WriteLine("deleted successfully");
+                    }
 
                 }
                 Console.WriteLine("not exists");
@@ -116,6 +117,19 @@ namespace AddressBook
             else
                 Console.WriteLine("list is empty,cannot delete");
         }
+        
+
+        public void AddAddressBook(string bookName)
+        {
+            AddressBookBuider addressBook = new AddressBookBuider();
+            dictionary.Add(bookName, addressBook);
+            Console.WriteLine("AddressBook Created.");
+        }
+        public Dictionary<string, AddressBookBuider> GetAddressBook()
+        {
+            return dictionary;
+        }
+
 
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -8,16 +8,36 @@ namespace AddressBook
     {
         public static void Main(string[] args)
         {
-            int option;
+            int option, option1;
+            string bookName = "default";
             AddressBookBuider addressBook = new AddressBookBuider();
+
+            Console.WriteLine("1. Would you like to work on existing addressbook ? if yes press 1");
+            Console.WriteLine("2. Would you like to add  new addressbook ? if yes press 2");
+            option1 = Convert.ToInt32(Console.ReadLine());
+            switch(option1)
+            {
+                case 1:
+                    addressBook.AddAddressBook(bookName);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Name Of New Addressbook You want to create : ");
+                    bookName = Console.ReadLine();
+                    addressBook.AddAddressBook(bookName);
+                    break;
+            }
+
+
             do
             {
-                
+                Console.WriteLine($"you are working on {bookName} addressbook ");
                 Console.WriteLine("Enter your option :");
                 Console.WriteLine("1. Add new contact ");
                 Console.WriteLine("2. Display the contacts");
                 Console.WriteLine("3. Edit  contacts");
                 Console.WriteLine("4. Delete  contacts");
+                Console.WriteLine("5. Add new addressbook");
+                Console.WriteLine("6. Switch Addressbook");
                 option = Convert.ToInt32(Console.ReadLine());
 
                 switch (option)
@@ -56,8 +76,37 @@ namespace AddressBook
                         addressBook.DeleteContact(NameToDelete);
                         break;
 
-                    default:
-                            Console.WriteLine("Wrong input");
+                    case 5: Console.WriteLine("Enter Name For New AddressBook");
+                            string newAddressBook = Console.ReadLine();
+                            addressBook.AddAddressBook(newAddressBook);
+                            Console.WriteLine("Would you like to Switch to " + newAddressBook);
+                            Console.WriteLine("1.Yes \n2.No");
+                            int c = Convert.ToInt32(Console.ReadLine());
+                            if (c == 1)
+                            {
+                                bookName = newAddressBook;
+                            }
+                            break;
+                    case 6:
+                            Console.WriteLine("Enter Name Of AddressBook From Below List");
+                            foreach (KeyValuePair<string, AddressBookBuider> item in addressBook.GetAddressBook())
+                            {
+                                Console.WriteLine(item.Key);
+                            }
+                            while (true)
+                            {
+                                bookName = Console.ReadLine();
+                                if (addressBook.GetAddressBook().ContainsKey(bookName))
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("No such AddressBook found. Try Again.");
+                                }
+                            }
+                            break;
+                    default: Console.WriteLine("wrong input");
                             break;
                 }
                 Console.WriteLine("Do you want to continue?  press 1 if yes,press 0 for Exit");
