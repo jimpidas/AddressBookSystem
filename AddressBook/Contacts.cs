d45bf86ecb00984312a85162c6939514ac150628
unknown <jimpidas>
2021-05-18 19:14:13 +0530

    [Jyosmita]Added:Code to delete a person using firstname

--- AddressBook/Contacts.cs
+++ AddressBook/Contacts.cs
@@ -6,6 +6,7 @@ namespace AddressBook
 {
     public class Contacts
     {
+       
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
